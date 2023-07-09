c94facddc56118f0a5e6bb669dab344de63952d0
Danil <fenikslife12345@gmail.com>
2023-07-09 15:38:55 +0300

    3 lesson, class Serializer

--- women/views.py
+++ women/views.py
@@ -10,29 +10,22 @@ from .serializers import WomenSerializer
 
 class WomenAPIView(APIView):
     def get(self, request):
-        lst1 = Women.objects.all().values()
-        lst = Women.objects.all()
-        for i in lst:
-            print(f"{i.return_that_what_i_want()}")
-        return Response({'posts': list(lst1)})
+        w = Women.objects.all()
+        return Response({'posts': WomenSerializer(w, many=True).data})
 
     def post(self, request):
+        serializer = WomenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
         post_new = Women.objects.create(
             title=request.data['title'],
             content=request.data['content'],
             cat_id=request.data['cat_id']
         )
 
-        return Response({'post': model_to_dict(post_new)})
+        return Response({'post': WomenSerializer(post_new).data})
 
-    # {
-    #     "title": "title_test",
-    #     "content": "content_test",
-    #     "cat_id": 2
-    # }
 
 # class WomenAPIView(generics.ListAPIView):
-#     # queryset = Women.objects.all()
-#     queryset = [i.return_that_what_i_want() for i in Women.objects.all()]
-#     # print(f"{queryset_gen=}")
+#     queryset = Women.objects.all()
 #     serializer_class = WomenSerializer
