c6d5f3578539c7bff4b508d7b6503d8c2026acd5
Danil <fenikslife12345@gmail.com>
2023-07-23 11:03:23 +0300

    11 lesson, authentication base on sessions

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -23,6 +23,7 @@ from rest_framework import routers
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/adr-auth/', include('rest_framework.urls')), # login based on session
     path('api/v1/women/', WomenApiList.as_view()),
     path('api/v1/women/<int:pk>/', WomenApiUpdate.as_view()),
     path('api/v1/womendelete/<int:pk>/', WomenApiDestroy.as_view()),
